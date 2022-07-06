a8ed630637a75b4041b22cfd722ff6ccfa1e97e0
sabogal <104802999+sabogal@users.noreply.github.com>
2022-07-06 16:31:28 -0500

    modelo base

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    
+    id = models.AutoField(primary_key= True)
+    state = models.BooleanField('Estado', default = True)
+    create_date = models.DateField('Fecha de Creacion', auto_now=False, auto_now_add=True)
+    modify_date = models.DateField('Fecha de Modificacion', auto_now=True, auto_now_add=False)
+    delete_date = models.DateField('Fecha de Eliminacion', auto_now=True, auto_now_add=False)
+    class Meta:
+        abstract = True
+        verbose_name = 'BaseModel'
+        verbose_name_plural = 'BaseModels'
+        
+    def __str__(self):
+        pass
+    
