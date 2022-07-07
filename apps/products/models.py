c3f72feddd3974a42d85e8590d45b92025cb5473
sabogal <104802999+sabogal@users.noreply.github.com>
2022-07-06 21:29:02 -0500

    modelo app

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,21 @@
+from pydoc import describe
 from django.db import models
+from simple_history.models import HistoricalRecords
+from apps.base.models import BaseModel
 
 # Create your models here.
+
+class MeasureUnit(BaseModel):
+    
+    descripcion = models.CharField('Descripcion', max_length = 50, blank=False ,null=False )
+    historical = HistoricalRecords()
+
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
