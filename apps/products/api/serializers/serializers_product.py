8eeaf62996a74d8c3df8197e3cdaec7391ccb5e3
sabogal <104802999+sabogal@users.noreply.github.com>
2022-07-08 08:17:56 -0500

    listar FK en products

--- apps/products/api/serializers/serializers_product.py
+++ apps/products/api/serializers/serializers_product.py
@@ -1,8 +1,29 @@
 
-from apps.products.models import Product
+from apps.products.models import MeasureUnit, Product
 from rest_framework import serializers
-
+from apps.products.api.serializers.serializers_general import MeasureUnitSerializer,CategoryProductSerializer
 class ProductSerializer(serializers.ModelSerializer):
+
+### primera opcion para mostrar los valores de las llaves foraneas
+    #measure_unit = MeasureUnitSerializer()
+    #category_product = CategoryProductSerializer()
+########
+### segunda opcion para mostrar los valores de las llaves foraneas
+## Se le llama los campos de el metodo Meta en el campo __str__
+    #measure_unit = serializers.StringRelatedField()
+    #category_product= serializers.StringRelatedField()
+########
     class Meta:
         model = Product
         exclude = ('state','create_date','modify_date','delete_date')
+
+### primera opcion para mostrar los valores de las llaves foraneas
+
+    def to_representation(self, instance):
+        return{
+            'id': instance.id,
+            'description': instance.description,
+            'image': instance.image if instance.image != '' else '',
+            'measure_unit': instance.measure_unit.descripcion,
+            'category_product': instance.category_product.descripcion,
+        }
